28fdf0cd870596e23282c7f070e0204eb03aad39
MarkMutt <lleasy8@gmail.com>
2023-01-12 18:25:34 -0500

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,14 @@
+
+export interface IHomePage {
+
+}
+
+export default function HomePage( ) {
+  
+  return (
+    <article className="prose prose-xl">
+      <h1>Welcome to Home Page</h1>
+      <p>This is the  HomePage</p>
+    </article>
+  );
+}
